6d43a98428ca23e201af3c6050d3035e9a0c7782
Stefan Kolesnikowicz <stefan.kolesnikowicz@achievers.com>
2021-02-08 23:55:17 -0500

    fixes

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 
 variable "do_token" {}
 variable "do_ssh_fingerprint" {}
-
+variable "do_pvt_key" {}
 
 provider "digitalocean" {
   token = var.do_token
@@ -23,20 +23,37 @@ data "digitalocean_ssh_key" "key" {
 
 # Create a new Web Droplet running ubuntu in the FRA1 region
 resource "digitalocean_droplet" "server" {
-    image  = "ubuntu-16-04-x64"
-    name   = "server"
+    image  = "ubuntu-20-04-x64"
+    name   = "echo"
     region = "fra1"
-    size   = "1gb"
+    size   = "s-1vcpu-1gb"
     ssh_keys =[
-      "${var.do_ssh_fingerprint}"
+      data.digitalocean_ssh_key.key.id
     ]
+    connection {
+      host = self.ipv4_address
+      user = "root"
+      type = "ssh"
+      private_key = file(var.do_pvt_key)
+      timeout = "2m"
+    }
+    provisioner "remote-exec" {
+      inline = [
+        "export PATH=$PATH:/usr/bin",
+        # install nginx
+        "export EMAIL=izmennik01@gmail.com",
+        "git clone https://github.com/izmennik01/scripts.git",
+        "./scripts/bootstrap.sh",
+        #"./scripts/home-sync.sh",
+      ]
+  }
 }
 
 # gimmie ip
 output "digitalocean_droplet_ip" {
-  value = "${digitalocean_droplet.server.ipv4_address}"
+  value = digitalocean_droplet.server.ipv4_address
 }
 
 output "digitalocean_droplet_name" {
-  value = "${digitalocean_droplet.server.name}"
+  value = digitalocean_droplet.server.name
 }
